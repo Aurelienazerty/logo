e3bcf62506bc53f28b12e27ca39218d182213051
Aurelienazerty <aurelienwillem@hotmail.com>
2015-02-22 14:37:51 +0100

    1.0
    Modifications pour avoir le logo de la team-azerty dans phpBB 3.1

---
+++ ext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package Logo.
+* @version $$
+* @copyright (c) 2015 www.team-azerty.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace Aurelienazerty\logo;
+
+/**
+* ext Class
+* www.team-azerty.com
+* @package Logo
+*/
+class ext extends \phpbb\extension\base
+{
+}
