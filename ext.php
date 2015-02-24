1e1cfad70dc8f9c4883b29b6df203e56e424e83b
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2015-02-24 14:55:54 +0100

    Update ext.php

--- ext.php
+++ ext.php
@@ -1,19 +1,19 @@
 <?php
 /**
 *
-* @package Logo.
+* @package StyleTA.
 * @version $$
 * @copyright (c) 2015 www.team-azerty.com
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace Aurelienazerty\logo;
+namespace Aurelienazerty\styleTA;
 
 /**
 * ext Class
 * www.team-azerty.com
-* @package Logo
+* @package StyleTA
 */
 class ext extends \phpbb\extension\base
 {
